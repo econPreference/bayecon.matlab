60c1b9940da279e56462dff2e883401f4aa20374
Preference <econ.preference@gmail.com>
2023-04-14 16:51:11 +0900

    fixed untracked files

--- State Space Model/codegen/mex/Time_Varying_Parameter/setEnv.bat
+++
@@ -1,4 +0,0 @@
-set PATH=C:\ProgramData\MATLAB\SupportPackages\R2020a\3P.instrset\mingw_w64.instrset\bin;C:\Program Files\MATLAB\R2020a\extern\include\win64;C:\Program Files\MATLAB\R2020a\extern\include;C:\Program Files\MATLAB\R2020a\simulink\include;C:\Program Files\MATLAB\R2020a\lib\win64;%MATLAB_BIN%;%PATH%
-set INCLUDE=C:\ProgramData\MATLAB\SupportPackages\R2020a\3P.instrset\mingw_w64.instrset\include;;%INCLUDE%
-set LIB=C:\ProgramData\MATLAB\SupportPackages\R2020a\3P.instrset\mingw_w64.instrset\lib;;%LIB%
-set LIBPATH=C:\Program Files\MATLAB\R2020a\extern\lib\win64;%LIBPATH%
--- Time Series/codegen/mex/Auto_Regression/SetEnv.bat
+++
@@ -1,4 +0,0 @@
-set PATH=C:\mingw-w64\x86_64-6.3.0-posix-seh-rt_v5-rev0\mingw64\bin;C:\Program Files\MATLAB\R2019b\extern\include\win64;C:\Program Files\MATLAB\R2019b\extern\include;C:\Program Files\MATLAB\R2019b\simulink\include;C:\Program Files\MATLAB\R2019b\lib\win64;%MATLAB_BIN%;%PATH%
-set INCLUDE=C:\mingw-w64\x86_64-6.3.0-posix-seh-rt_v5-rev0\mingw64\include;;%INCLUDE%
-set LIB=C:\mingw-w64\x86_64-6.3.0-posix-seh-rt_v5-rev0\mingw64\lib;;%LIB%
-set LIBPATH=C:\Program Files\MATLAB\R2019b\extern\lib\win64;%LIBPATH%
